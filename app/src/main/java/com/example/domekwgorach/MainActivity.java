e0c8919004330300e6a7418376777d1db6028565
BartekK022 <baklu100508@gmail.com>
2025-12-12 14:27:48 +0100

    cala aplikacja i czesc funkcji

--- app/src/main/java/com/example/domekwgorach/MainActivity.java
+++ app/src/main/java/com/example/domekwgorach/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.domekwgorach;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int i = 0;
+    public void Polub() {
+        i++;
+    }
+    public void  usun() {
+        if(i <= 0) {
+        } else {
+            i--;
+        }
+    }
+
 }
